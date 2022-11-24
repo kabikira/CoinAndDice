7db6ee432e7f293bfa11bb0439b505b8dc642ba5
kabikira <kabikirar2@protonmail.com>
2022-11-24 23:09:14 +0900

    Initial Commit

---
+++ CoinAndDice/CoinAndDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinAndDiceApp.swift
+//  CoinAndDice
+//
+//  Created by koala panda on 2022/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinAndDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
